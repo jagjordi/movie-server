f0380a2c037608497f1ce84a784383d7a7e995c8
Jordi Surface Ubuntu <jordialtayo@gmail.com>
2018-10-20 19:01:12 +0200

    mdf

--- movie-server.py
+++ movie-server.py
@@ -1,8 +1,12 @@
 import gspread
-from oauth2client import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet-220015-83b4ef286d02.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet-220015-58b6c82bc04b.json', scope)
 
+gc = gspread.authorize(credentials)
 
+wks = gc.open_by_key('1fith6AF1l9Ws_8nthr-xaCaR_4_Fv2QyR5ePA_2lwpM').sheet1
+
+print(wks.get_all_records)
